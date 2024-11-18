8db3b710148868018503fd8661b1a2ba278216f9
majok31 <majoktot70@gmail.com>
2024-11-18 13:22:05 -0800

    React state components

--- 0x05-react_state/task_2/dashboard/src/App/App.js
+++ 0x05-react_state/task_2/dashboard/src/App/App.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
@@ -7,15 +6,26 @@ import CourseList from "../CourseList/CourseList";
 import Notifications from "../Notifications/Notifications";
 import BodySectionWithMarginBottom from "../BodySection/BodySectionWithMarginBottom";
 import BodySection from "../BodySection/BodySection";
-import "./App.css";
+import { StyleSheet, css } from "aphrodite";
 import PropTypes from "prop-types";
 import { getLatestNotification } from "../utils/utils";
+import { AppContext, user } from "./AppContext";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      displayDrawer: false,
+      user: user,
+      logOut: this.logOut,
+    };
+
     this.handleKeyPress = this.handleKeyPress.bind(this);
+    this.handleDisplayDrawer = this.handleDisplayDrawer.bind(this);
+    this.handleHideDrawer = this.handleHideDrawer.bind(this);
+    this.logIn = this.logIn.bind(this);
+    this.logOut = this.logOut.bind(this);
   }
 
   listCourses = [
@@ -37,6 +47,15 @@ class App extends React.Component {
       this.props.logOut();
     }
   }
+
+  handleDisplayDrawer() {
+    this.setState({ displayDrawer: true });
+  }
+
+  handleHideDrawer() {
+    this.setState({ displayDrawer: false });
+  }
+
   componentDidMount() {
     document.addEventListener("keydown", this.handleKeyPress);
   }
@@ -45,35 +64,72 @@ class App extends React.Component {
     document.removeEventListener("keydown", this.handleKeyPress);
   }
 
+  logIn(email, password) {
+    this.setState({
+      user: {
+        email,
+        password,
+        isLoggedIn: true,
+      },
+    });
+  }
+
+  logOut() {
+    this.setState({
+      user: user,
+    });
+  }
+
   render() {
     return (
-      <React.Fragment>
-        <div className="App">
-          <div className="heading-section">
-            <Notifications listNotifications={this.listNotifications} />
-            <Header />
+      <AppContext.Provider
+        value={{
+          user: this.state.user,
+          logout: this.state.logOut,
+        }}
+      >
+        <React.Fragment>
+          <div className={css(styles.App)}>
+            <div className="heading-section">
+              <Notifications
+                listNotifications={this.listNotifications}
+                displayDrawer={this.state.displayDrawer}
+                handleDisplayDrawer={this.handleDisplayDrawer}
+                handleHideDrawer={this.handleHideDrawer}
+              />
+              <Header />
+            </div>
+            {this.state.user.isLoggedIn ? (
+              <BodySectionWithMarginBottom title="Course list">
+                <CourseList listCourses={this.listCourses} />
+              </BodySectionWithMarginBottom>
+            ) : (
+              <BodySectionWithMarginBottom title="Log in to continue">
+                <Login logIn={this.logIn} />
+              </BodySectionWithMarginBottom>
+            )}
+            <BodySection title="News from the school">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis at tempora odio, necessitatibus repudiandae reiciendis cum nemo sed asperiores ut molestiae eaque aliquam illo
+                ipsa iste vero dolor voluptates.
+              </p>
+            </BodySection>
+            <Footer />
           </div>
-          {this.props.isLoggedIn ? (
-            <BodySectionWithMarginBottom title="Course list">
-              <CourseList listCourses={this.listCourses} />
-            </BodySectionWithMarginBottom>
-          ) : (
-            <BodySectionWithMarginBottom title="Log in to continue">
-              <Login />
-            </BodySectionWithMarginBottom>
-          )}
-          <BodySection title="News from the school">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis at tempora odio, necessitatibus repudiandae reiciendis cum nemo sed asperiores ut molestiae eaque aliquam illo ipsa
-              iste vero dolor voluptates.
-            </p>
-          </BodySection>
-          <Footer />
-        </div>
-      </React.Fragment>
+        </React.Fragment>
+      </AppContext.Provider>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  App: {
+    height: "100vh",
+    maxWidth: "100vw",
+    position: "relative",
+    fontFamily: "Arial, Helvetica, sans-serif",
+  },
+});
 
 App.defaultProps = {
   isLoggedIn: false,
@@ -88,4 +144,3 @@ App.propTypes = {
 };
 
 export default App;
-
--- 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
+++ 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
@@ -1,15 +1,27 @@
-import React from "react";
+
+import React, { useContext } from "react";
 import "./Footer.css";
 import { getFullYear, getFooterCopy } from "../utils/utils";
+import { AppContext } from "../App/AppContext";
 
 function Footer() {
+  const { user } = useContext(AppContext);
+
   return (
     <>
       <div className="App-footer">
-        Copyright {getFullYear()} - {getFooterCopy()}
+        {user.isLoggedIn && (
+          <p>
+            <a href="#">Contact us</a>
+          </p>
+        )}
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy()}
+        </p>
       </div>
     </>
   );
 }
 
 export default Footer;
+
